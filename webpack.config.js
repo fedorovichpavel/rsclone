168c765a36c40e8fc613fdb957d58f03984ff8f6
Mikhail Hursky <kakai747o@gmail.com>
2020-12-27 15:39:43 +0300

    feat: default setting project

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const dev = 'development';
+
+module.exports = {
+    mode: dev,
+    entry: {
+        main: path.resolve(__dirname, './src/script/index.ts'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/icon/favicon.png'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'],
+            },
+            {
+                test: /\.(ogg|mp3|wav|mpe?g)$/i,
+                use: 'file-loader',
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|svg|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // CSS, Sass
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(ttf|woff|woff2)$/,
+                use: ['file-loader'],
+            },
+        ],
+
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    }
+};
