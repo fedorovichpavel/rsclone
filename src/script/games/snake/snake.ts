b20b096a127b1f1fbca9e7e8fd0718b42f34f036
Maryna Migacheva <m.migacheva@gmail.com>
2021-01-20 21:56:24 +0300

    fix: algorithm game

--- src/script/games/snake/snake.ts
+++ src/script/games/snake/snake.ts
@@ -18,9 +18,21 @@ type MySnake = {
   sprite: Phaser.GameObjects.Sprite,
 }
 
-import { settings } from './settingssnake';
-
 export default class Snake extends Phaser.Scene {
+  private gameWidth: number;
+
+  private gameHeight: number;
+
+  private boardSize: number;
+
+  private boardSizeX: number;
+
+  private boardSizeY: number;
+
+  private cellSize: number;
+
+  private speed: number;
+
   private delay: number;
 
   private mice: any;
@@ -33,6 +45,10 @@ export default class Snake extends Phaser.Scene {
 
   private direction: string;
 
+  private numberFlowers: number;
+
+  private startMouseNumber: number;
+
   private score: number;
 
   private tableScore: Phaser.GameObjects.Text;
@@ -41,243 +57,284 @@ export default class Snake extends Phaser.Scene {
 
   private swipe: Swipe;
 
-    constructor() {
-        super({
-          key: "SnakeGame"
+  constructor() {
+    super({
+      key: 'SnakeGame',
+    });
+    this.Memory = new Memory();
+  }
+
+  create() {
+    this.boardSize = 20;
+    this.boardSizeX = 11;
+    this.boardSizeY = 23;
+    this.cellSize = 20;
+    this.gameHeight = this.cellSize * this.boardSizeY;
+    this.gameWidth = this.cellSize * this.boardSizeX;
+    this.speed = 500;
+
+    this.mice = [];
+    this.flowers = [];
+    this.numberFlowers = 1;
+    this.startMouseNumber = 1;
+    this.delay = this.speed;
+    this.offsets = {
+      up: { x: 0, y: -1 },
+      down: { x: 0, y: 1 },
+      left: { x: -1, y: 0 },
+      right: { x: 1, y: 0 },
+    };
+    this.direction = 'right';
+
+    this.add.tileSprite(
+      this.gameWidth / 2,
+      this.gameHeight / 2,
+      this.gameWidth,
+      this.gameHeight,
+      'ground',
+    );
+
+    this.snake = [
+      {
+        x: 0, y: 0, name: 'tail', direction: 'right', sprite: undefined,
+      },
+      {
+        x: 1, y: 0, name: 'body', direction: 'right', sprite: undefined,
+      },
+      {
+        x: 2, y: 0, name: 'head', direction: 'right', sprite: undefined,
+      },
+    ];
+    this.snake.forEach((item) => {
+      const [dataX, dataY] = this.getXY(item.x, item.y);
+      const elem = item;
+      elem.sprite = this.add.sprite(dataX, dataY, elem.name);
+      return elem;
+    });
+
+    this.createMice();
+    this.createFlowers();
+
+    this.input.keyboard.on('keydown', this.handleKey, this);
+
+    this.score = 0;
+    this.tableScore = this.add.text(22, 0, `Score: ${this.score}`, style);
+  }
+
+  update(time, delta) {
+    this.tableScore.text = `Score:${this.score}`;
+    this.delay -= delta;
+
+    if (this.delay <= 0) {
+      this.delay = this.speed - this.delay;
+
+      const first = this.snake[this.snake.length - 1];
+      first.name = 'body';
+      first.sprite.setTexture(first.name);
+
+      const x = this.convertX(first.x + this.offsets[this.direction].x);
+      const y = this.convertY(first.y + this.offsets[this.direction].y);
+
+      if (this.snake.some((s) => s.x === x && s.y === y)) {
+        this.gameOver();
+        return;
+      }
+
+      if (this.flowers.some((f) => f.x === x && f.y === y)) {
+        this.gameOver();
+        return;
+      }
+
+      const [dataX, dataY] = this.getXY(x, y);
+
+      const angles = {
+        up: -90,
+        down: 90,
+        right: 0,
+        left: 180,
+      };
+
+      const head = {
+        x,
+        y,
+        name: 'head',
+        direction: this.direction,
+        sprite: undefined,
+      };
+      head.sprite = this.add.sprite(dataX, dataY, head.name);
+      head.sprite.angle = angles[this.direction];
+      this.snake.push(head);
+
+      const eaten = this.mice.filter((m) => m.x === head.x && m.y === head.y);
+
+      if (eaten.length > 0) {
+        this.score += 1;
+        this.mice = this.mice.filter((m) => m.x !== head.x || m.y !== head.y);
+        eaten.forEach((item) => {
+          item.sprite.destroy();
         });
-        this.Memory = new Memory();
+      } else {
+        const drop = this.snake.shift();
+        drop.sprite.destroy();
+        const last = this.snake[0];
+        last.name = 'tail';
+        last.sprite.setTexture(last.name);
+      }
     }
 
-    create() {
-        this.mice = [];
-        this.flowers = [];
-        this.delay = settings.delay;
-        this.offsets = {
-            up: {x: 0, y: -1},
-            down: {x: 0, y: 1},
-            left: {x: -1, y: 0},
-            right: {x: 1, y: 0},
-        };
-        this.direction = 'right';
+    this.levelUp();
 
-        this.add.tileSprite(
-              settings.gameWidth / 2,
-              settings.gameHeight / 2,
-              settings.gameWidth,
-              settings.gameHeight,
-              'ground');
+    if (this.mice.length < this.startMouseNumber) {
+      this.createMice();
+    }
+  }
 
-        this.snake = [
-            {x: 0, y: 0, name: 'tail', direction: 'right', sprite: undefined},
-            {x: 1, y: 0, name: 'body', direction: 'right', sprite: undefined},
-            {x: 2, y: 0, name: 'head', direction: 'right', sprite: undefined},
-        ];
-        this.snake.forEach((item, index) => {
-            let [worldX, worldY] = this.cellToWorld(item.x, item.y);
-            item.sprite = this.add.sprite(worldX, worldY, item.name);
+  levelUp() {
+    if (this.score === 3) {
+      this.speed = 400;
+    }
+    if (this.score === 5) {
+      this.startMouseNumber = 2;
+    }
+    if (this.score === 10) {
+      this.numberFlowers = 2;
+      while (this.flowers.length < this.numberFlowers) {
+        this.createFlowers();
+      }
+    }
+    if (this.score === 15) {
+      this.speed = 300;
+      this.startMouseNumber = 3;
+      this.numberFlowers = 4;
+      while (this.flowers.length < this.numberFlowers) {
+        this.createFlowers();
+      }
+    }
+    if (this.score === 20) {
+      this.numberFlowers = 5;
+      while (this.flowers.length < this.numberFlowers) {
+        this.createFlowers();
+      }
+    }
+    if (this.score === 25) {
+      this.speed = 250;
+    }
+  }
+
+  convertX(i) {
+    return (this.boardSizeX + Math.sign(i) * Math.abs(i)) % this.boardSizeX;
+  }
+
+  convertY(i) {
+    return (this.boardSizeY + Math.sign(i) * Math.abs(i)) % this.boardSizeY;
+  }
+
+  createFlowers() {
+    let created = false;
+    do {
+      const x = Math.floor(Math.random() * this.boardSizeX);
+      const y = Math.floor(Math.random() * this.boardSizeY);
+
+      if (!this.mice.some((m) => m.x === x && m.y === y)
+                && !this.flowers.some((f) => f.x === x && f.y === y)
+                && !this.snake.some((s) => s.x === x && s.y === y)) {
+        created = true;
+
+        const [dataX, dataY] = this.getXY(x, y);
+        this.flowers.push({
+          x,
+          y,
+          sprite: this.add.sprite(dataX, dataY, 'flower'),
         });
+      }
+    } while (!created);
+  }
 
+  createMice() {
+    while (this.mice.length < this.startMouseNumber) {
+      const x = Math.floor(Math.random() * this.boardSizeX);
+      const y = Math.floor(Math.random() * this.boardSizeY);
 
-        this.createMice();
-
-        let i = 0;
-        while(this.flowers.length < settings.numberFlowers) {
-            i++;
-            this.createFlowers();
-        }
-
-        this.input.keyboard.on('keydown', this.handleKey, this);
-
-        this.score = 0;
-        this.tableScore = this.add.text(22, 0, `Score: ${this.score}`, style);
-    }
-
-    createFlowers() {
-        let created = false;
-        do {
-            let x = Math.floor(Math.random() * settings.boardSizeX),
-                y = Math.floor(Math.random() * settings.boardSizeY);
-
-            if (!this.mice.some(f => f.x == x && f.y == y)
-                && !this.flowers.some(s => s.x == x && s.y == y)
-                && !this.snake.some(s => s.x == x && s.y == y)) {
-
-                created = true;
-
-                let [worldX, worldY] = this.cellToWorld(x, y);
-                this.flowers.push({
-                    x, y,
-                    sprite: this.add.sprite(worldX, worldY, 'flower'),
-                })
-            }
-        } while(!created);
-    }
-
-    update(time, delta) {
-        this.tableScore.text = `Score:${this.score}`;
-        this.delay -= delta;
-
-        if (this.delay <= 0) {
-            this.delay = settings.delay - this.delay;
-
-            let first = this.snake[this.snake.length - 1];
-            first.name = 'body';
-            first.sprite.setTexture(first.name);
-
-            let x = this.convertX(first.x + this.offsets[this.direction].x);
-            let y = this.convertY(first.y + this.offsets[this.direction].y);
-
-            if (this.snake.some(s => s.x == x && s.y == y)) {
-                this.gameOver();
-                return;
-            }
-
-            if (this.flowers.some(s => s.x == x && s.y == y)) {
-                this.gameOver();
-                return;
-            }
-
-            let [worldX, worldY] = this.cellToWorld(x, y);
-
-            const angles = {
-                'up': -90,
-                'down': 90,
-                'right': 0,
-                'left': 180,
-            };
-
-            let head = {
-                x, y,
-                name: 'head',
-                direction: this.direction,
-                sprite: undefined,
-            };
-            head.sprite = this.add.sprite(worldX, worldY, head.name);
-            head.sprite.angle = angles[this.direction];
-            this.snake.push(head);
-
-            let eaten = this.mice.filter(f => f.x == head.x && f.y == head.y);
-
-            if (eaten.length > 0) {
-                console.log('eat mouse!');
-                this.score += 1;
-                this.mice = this.mice.filter(f => f.x != head.x || f.y != head.y);
-                eaten.forEach(f => {
-                    f.sprite.destroy();
-                });
-
-            }
-            else {
-                let drop = this.snake.shift();
-                drop.sprite.destroy();
-                let last = this.snake[0];
-                last.name = 'tail';
-                last.sprite.setTexture(last.name);
-            }
-        }
-
-
-        if (this.mice.length < settings.minFoodNumber) {
-            this.createMice();
-        }
-    }
-
-    convert(i) {
-        return (settings.boardSize + Math.sign(i) * Math.abs(i)) % settings.boardSize;
-    }
-
-    convertX(i) {
-        return (settings.boardSizeX + Math.sign(i) * Math.abs(i)) % settings.boardSizeX;
-    }
-
-    convertY(i) {
-        return (settings.boardSizeY + Math.sign(i) * Math.abs(i)) % settings.boardSizeY;
-    }
-
-
-    createMice() {
-        while (this.mice.length < settings.maxFoodNumber) {
-
-            let x = Math.floor(Math.random() * settings.boardSizeX),
-                y = Math.floor(Math.random() * settings.boardSizeY);
-
-            if (!this.mice.some(f => f.x == x && f.y == y) && !this.snake.some(s => s.x == x && s.y == y)) {
-                let [foodX, foodY] = this.cellToWorld(x, y);
-                this.mice.push({
-                    x, y,
-                    sprite: this.add.sprite(foodX, foodY, 'mouse'),
-                })
-            }
-        }
-    }
-
-    reverse() {
-        const inverse = {
-            up: 'down',
-            down: 'up',
-            left: 'right',
-            right: 'left',
-        };
-
-        this.snake = this.snake.reverse();
-        this.snake.forEach(s => {
-            s.direction = inverse[s.direction];
+      if (!this.mice.some((m) => m.x === x && m.y === y)
+          && !this.flowers.some((f) => f.x === x && f.y === y)
+          && !this.snake.some((s) => s.x === x && s.y === y)) {
+        const [dataX, dataY] = this.getXY(x, y);
+        this.mice.push({
+          x,
+          y,
+          sprite: this.add.sprite(dataX, dataY, 'mouse'),
         });
-        this.direction = this.snake[this.snake.length - 1].direction;
+      }
     }
+  }
 
-    handleKey(e) {
-        switch (e.code) {
-            case 'ArrowLeft':
-                if (this.direction == 'right') {
-                    this.reverse();
-                }
-                else {
-                    this.direction = 'left';
-                }
-                break;
-            case 'ArrowRight':
-                if (this.direction == 'left') {
-                    this.reverse();
-                }
-                else {
-                    this.direction = 'right';
-                }
-                break;
-            case 'ArrowUp':
-                if (this.direction == 'down') {
-                    this.reverse();
-                }
-                else {
-                    this.direction = 'up';
-                }
-                break;
-            case 'ArrowDown':
-                if (this.direction == 'up') {
-                    this.reverse();
-                }
-                else {
-                    this.direction = 'down';
-                }
-                break;
+  reverse() {
+    const inverse = {
+      up: 'down',
+      down: 'up',
+      left: 'right',
+      right: 'left',
+    };
+
+    this.snake = this.snake.reverse();
+    this.snake.forEach((s) => {
+      const elem = s;
+      elem.direction = inverse[elem.direction];
+      return elem;
+    });
+    this.direction = this.snake[this.snake.length - 1].direction;
+  }
+
+  handleKey(e) {
+    switch (e.code) {
+      case 'ArrowLeft':
+        if (this.direction === 'right') {
+          this.reverse();
+        } else {
+          this.direction = 'left';
         }
+        break;
+      case 'ArrowRight':
+        if (this.direction === 'left') {
+          this.reverse();
+        } else {
+          this.direction = 'right';
+        }
+        break;
+      case 'ArrowUp':
+        if (this.direction === 'down') {
+          this.reverse();
+        } else {
+          this.direction = 'up';
+        }
+        break;
+      case 'ArrowDown':
+        if (this.direction === 'up') {
+          this.reverse();
+        } else {
+          this.direction = 'down';
+        }
+        break;
+      default:
+        break;
     }
+  }
 
-    cellToWorld(x, y) {
-        return [
-            x * settings.cellSize + settings.cellSize / 2,
-            y * settings.cellSize + settings.cellSize / 2
-        ];
-    }
+  getXY(x, y) {
+    return [
+      x * this.cellSize + this.cellSize / 2,
+      y * this.cellSize + this.cellSize / 2,
+    ];
+  }
 
-    gameOver() {
-      this.scene.pause();
-      this.Memory.setScorePoint(this.score);
-      this.Memory.setPrevGame('SnakeGame');
-      setTimeout(() => {
-        this.scene.restart(this);
-        this.scene.stop();
-        this.scene.start('GameOver');
-      }, 1000);
-    }
+  gameOver() {
+    this.scene.pause();
+    this.Memory.setScorePoint(this.score);
+    this.Memory.setPrevGame('SnakeGame');
+    setTimeout(() => {
+      this.scene.restart(this);
+      this.scene.stop();
+      this.scene.start('GameOver');
+    }, 1000);
+  }
 }
